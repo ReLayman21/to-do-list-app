4a9065eda2b9db786ce79fe180c3ca733af584ac
ReLayman21 <94310727+ReLayman21@users.noreply.github.com>
2023-01-16 15:54:16 -0500

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let element = $("#my-class");
+
+let inputs = $("#inputs");
+inputs.addClass("my-class");
+
+let list = $("#list");
+list.append("<li>new item</li>");
+
+let li = $("<li></li>");
+li.append("li");
