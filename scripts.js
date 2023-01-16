56cd260e34c8a71d3ec3fab703c8c2c321eb47cb
ReLayman21 <94310727+ReLayman21@users.noreply.github.com>
2023-01-16 16:06:08 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 let element = $("#my-class");
 
-let inputs = $("#inputs");
+let inputs = $("#input");
 inputs.addClass("my-class");
 
 let list = $("#list");
 list.append("<li>new item</li>");
 
 let li = $("<li></li>");
-li.append("li");
+let inputValue = $("#input").val();
+li.append(inputValue);
 
 let button = $("#button");
 $("#button").on("click", function (event) {
@@ -15,11 +16,8 @@ $("#button").on("click", function (event) {
 });
 "#button".off("click");
 
-let inputValue = $("#inputs").val();
-li.append(inputValue);
-
 let newElement = $('<div class= "new-class"> Content is here!</div>');
 $("body").append(newElement);
 
 inputs.show();
-$("body").append(inputs);
+$("body").append(input);
