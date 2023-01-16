d377ccf878f34b557f04ff0e55e7589b63bf6e96
ReLayman21 <94310727+ReLayman21@users.noreply.github.com>
2023-01-16 16:24:40 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,36 @@
-let element = $("#my-class");
-
-let inputs = $("#input");
-inputs.addClass("my-class");
-
-let list = $("#list");
-list.append("<li>new item</li>");
-
-let li = $("<li></li>");
-let inputValue = $("#input").val();
-li.append(inputValue);
-
-let button = $("#button");
-$("#button").on("click", function (event) {
+function newItem() {
+  // Adding items to the list
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
   li.append(inputValue);
-});
-"#button".off("click");
 
-let newElement = $('<div class= "new-class"> Content is here!</div>');
-$("body").append(newElement);
+  //   Crossing out item from list
+  li.on("click", function () {
+    li.addClass("strike");
+  });
 
-inputs.show();
-$("body").append(input);
+  // Deleting item from list
+
+  // Chaning order of the list
+}
+
+//   let button = $("#button");
+//   $("#button").on("click", function (event) {
+//     li.append(inputValue);
+//   });
+//   "#button".off("click");
+
+//   let newElement = $('<div class= "new-class"> Content is here!</div>');
+//   $("body").append(newElement);
+
+//   inputs.show();
+//   $("body").append(input);
+// }
+
+//   $('input').each(function (i)){
+//     let inputValue= $(this).val();
+//     let inputName = $(this).attr('name');
+//     if(inputValue().length <1){
+//         alert("Please add item to To Do list!")
+//     }
+//   }
